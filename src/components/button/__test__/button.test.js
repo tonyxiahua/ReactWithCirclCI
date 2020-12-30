bbde2d223bbbccfd987d4197f5a3529e7c94eb7f
xhua006 <xia.hua.tony@iqhibattery.com>
2020-12-29 20:29:31 -0800

    test

---
+++ src/components/button/__test__/button.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from './../button';
+//import {isTSAnyKeyword} from '@babel/types'
+import { render , cleanup } from '@testing-library/react';
+//import "jest-dom/extend-expect";
+import '@testing-library/jest-dom';
+
+import renderer from "react-test-renderer"
+
+afterEach(cleanup);
+
+it("renders without crashing",()=>{
+    const div = document.createElement("div");
+    ReactDOM.render(<Button></Button>,div);
+})
+
+it("renders button correctly",()=>{
+    const {getByTestId} = render(<Button label= 'click me please'></Button>);
+    expect(getByTestId('button')).toHaveTextContent("click me please");
+})
+
+it("renders button correctly",()=>{
+    const {getByTestId} = render(<Button label= 'save'></Button>);
+    expect(getByTestId('button')).toHaveTextContent("save");
+})
+
+it("matches snapshot", () => {
+    const tree = renderer.create(<Button label = "save"></Button>).toJSON();
+    expect(tree).toMatchSnapshot();
+})
